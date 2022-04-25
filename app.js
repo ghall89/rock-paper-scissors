e5c8a765a6af01ba405d1d36f4bbf858091f43da
Graham Hall <ghall89@me.com>
2022-04-25 16:44:43 +0000

    player and cpu moves

--- app.js
+++ app.js
@@ -1,5 +1,17 @@
-const play = e => {
-	const clickedEl = e.target.id;
+const resultEl = document.querySelector('#result');
 
-	console.log(clickedEl);
+const choiceArr = ['rock', 'paper', 'scissors'];
+const resultArr = [' You win! 🎉', 'You lose. 😞'];
+
+const gameLogic = (you, cpu) => {
+	resultEl.innerText = `You chose ${you} and the cpu chose ${cpu}.`;
+};
+
+const cpuPlay = () => choiceArr[Math.floor(Math.random() * (2 - 0 + 1) + 0)];
+
+const play = e => {
+	const clickedId = e.target.id;
+	const cpuChoice = cpuPlay();
+
+	gameLogic(clickedId, cpuChoice);
 };
