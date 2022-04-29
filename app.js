6ff5b2e3f1f43ddfd7f38b21103b40b20826d5d0
Graham Hall <ghall89@me.com>
2022-04-29 13:57:36 +0000

    code cleanup

--- app.js
+++ app.js
@@ -4,8 +4,6 @@ const loadEl = document.querySelector('#loading');
 const playerScoreEl = document.querySelector('#player-score');
 const cpuScoreEl = document.querySelector('#cpu-score');
 
-const choiceArr = ['rock', 'paper', 'scissors'];
-
 let playerScore = 0;
 let cpuScore = 0;
 
@@ -17,33 +15,35 @@ const gameLogic = (you, cpu) => {
 
 	if (you === cpu) {
 		result = "It's a tie!";
+	} else if (
+		(you === 'rock' && cpu === 'scissors') ||
+		(you === 'paper' && cpu === 'rock') ||
+		(you === 'scissors' && cpu === 'paper')
+	) {
+		result = 'You win! 🎉';
+		playerScore++;
 	} else {
-		if (
-			(you === 'rock' && cpu === 'scissors') ||
-			(you === 'paper' && cpu === 'rock') ||
-			(you === 'scissors' && cpu === 'paper')
-		) {
-			result = 'You win! 🎉';
-			playerScore++;
-		} else {
-			result = 'You lose. 😞';
-			cpuScore++;
-		}
+		result = 'You lose. 😞';
+		cpuScore++;
 	}
 
-	resultEl.innerText = `You chose ${you} and the cpu chose ${cpu}. ${result}`;
-
-	console.log(`player: ${playerScore} | cpu: ${cpuScore}`);
 	playerScoreEl.innerText = playerScore;
 	cpuScoreEl.innerText = cpuScore;
+
+	resultEl.innerText = `You chose ${you} and the cpu chose ${cpu}. ${result}`;
 };
 
-const cpuPlay = () => choiceArr[Math.floor(Math.random() * (2 - 0 + 1) + 0)];
+const cpuPlay = () => {
+	const choiceArr = ['rock', 'paper', 'scissors'];
+	return choiceArr[Math.floor(Math.random() * (2 - 0 + 1) + 0)];
+};
 
 const play = e => {
-	const clickedId = e.target.id;
+	const playerChoice = e.target.id;
 	const cpuChoice = cpuPlay();
+
 	loadEl.style.display = 'block';
 	resultEl.style.display = 'none';
-	setTimeout(gameLogic, 1000, clickedId, cpuChoice);
+
+	setTimeout(gameLogic, 1000, playerChoice, cpuChoice);
 };
